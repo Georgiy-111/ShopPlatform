545d0caf2f8f1ab0164949880282a56d5d1e3fb0
georgiy-111 <ger900005@mail.ru>
2025-11-20 22:46:16 +0300

    Удалил лишнее в Program.cs всё что создается по дефолту, установил нужный nuget пакет и создал тестовый контроллер что бы проверить swagger, указал удобные порты.

---
+++ CatalogService/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var response = new
+        {
+            status = "OK",
+            service = "CatalogService",
+            time = DateTime.UtcNow
+        };
+
+        return Ok(response);
+    }
+}
