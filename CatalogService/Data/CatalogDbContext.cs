94b7436842327d60c15167bb5cae6d21244b49af
Georgiy <ger900005@mail.ru>
2025-11-24 20:23:44 +0700

    --

---
+++ CatalogService/Data/CatalogDbContext.cs
@@ -0,0 +1,44 @@
+﻿using CatalogService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogService.Data;
+
+public class CatalogDbContext : DbContext
+{
+    public CatalogDbContext(DbContextOptions<CatalogDbContext> options)
+        : base(options)
+    {
+    }
+
+    // Набор сущностей (таблица Products в БД)
+    public DbSet<Product> Products { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Конфигурация сущности Product
+        modelBuilder.Entity<Product>(entity =>
+        {
+            // Имя таблицы
+            entity.ToTable("Products");
+
+            // Первичный ключ
+            entity.HasKey(p => p.Id);
+
+            // Свойства
+            entity.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            entity.Property(p => p.Category)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(p => p.Price)
+                .HasColumnType("numeric(18,2)");
+
+            entity.Property(p => p.Stock);
+        });
+    }
+}
