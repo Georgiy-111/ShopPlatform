a9d0e5eeb86c26c2c5f2618cd8498a8086eeb70b
Georgiy <ger900005@mail.ru>
2025-11-24 22:00:39 +0700

    ~~~

--- CatalogService/Program.cs
+++ CatalogService/Program.cs
@@ -1,25 +1,27 @@
+using CatalogService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Подключаем контроллеры (они будут работать позже)
-builder.Services.AddControllers();
+var connectionString = builder.Configuration.GetConnectionString("Catalog");
 
-// Подключаем OpenAPI/Swagger (для просмотра API в браузере)
+builder.Services.AddDbContext<CatalogDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// В режиме разработки включаем Swagger UI
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Убираем HTTPS (работаем по HTTP)
 app.UseAuthorization();
 
-// Подключаем маршрутизацию контроллеров
 app.MapControllers();
 
 app.Run();
