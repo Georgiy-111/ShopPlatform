7ccfdc35e114250f02e5aa7f6c2acd6c62e1d8fd
Georgiy <ger900005@mail.ru>
2025-11-26 10:39:50 +0700

    ~~ Сделал все тоже самые действия как и для  двух предыдущих сервисов, отличие только в сущностях пока что,предварительная настройка подключения и проверка того что Docker работает, в adminer и swagger всё корректно отображается

---
+++ UserService/Data/UsersDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Domain.Entities;
+
+namespace UserService.Data;
+
+public class UsersDbContext : DbContext
+{
+    public UsersDbContext(DbContextOptions<UsersDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+}
--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,25 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using UserService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Подключаем контроллеры (они будут работать позже)
-builder.Services.AddControllers();
+var connectionString = builder.Configuration.GetConnectionString("Users");
 
-// Подключаем OpenAPI/Swagger (для просмотра API в браузере)
+builder.Services.AddDbContext<UsersDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// В режиме разработки включаем Swagger UI
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Убираем HTTPS (работаем по HTTP)
-app.UseAuthorization();
-
-// Подключаем маршрутизацию контроллеров
 app.MapControllers();
 
 app.Run();
